9d8a2542e1b6a38711a63f36e9e7b9e7e6dd121a
Ezaria <felipetiznado1@gmail.com>
2024-05-29 17:47:01 -0400

    firebase agregado

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,34 +1,58 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import arrayProducts from "./json/products.json";
+//import arrayProducts from "./json/products.json";
 import ItemList from "./ItemList";
 import Carousel from "./Carousel"
 import ItemDetail from "./ItemDetail";
+import { collection, doc, getDoc, getDocs, getFirestore, snapshotEqual} from "firebase/firestore";
+
+
 
 
 
 const ItemListContainer = () => {
     const [items, setItems] = useState([]);
     const {id} = useParams();
-
-    useEffect(() => {
-        const promesa = new Promise(resolve => {
-            setTimeout(() => {
-                resolve(id ? arrayProducts.filter(item => item.category == id): arrayProducts);
-            }, 1000)
-        });
+    
+//acceso a productos via JSON
+    // useEffect(() => {
+    //     const promesa = new Promise(resolve => {
+    //         setTimeout(() => {
+    //             resolve(id ? arrayProducts.filter(item => item.category == id): arrayProducts);
+    //         }, 1000)
+    //     });
         
-        promesa.then(respuesta => {
-            setItems(respuesta);
-        })
-    }, [id])
+    //     promesa.then(respuesta => {
+    //         setItems(respuesta);
+    //     })
+    // }, [id])
+
+// }, [])
+
+//Acceder a una collection en firestore
+
+useEffect(() => {
+    const db = getFirestore();
+    const itemsCollection = collection(db, 'productos');
+    getDocs(itemsCollection).then(snapShot => {
+        if ( snapShot.size >0){
+            setItems(snapShot.docs.map(item => (
+                {id: item.id, ...item.data()})));
+                
+        } 
+    });
+}, [])
+
+
+
 
     return (
         <>
             {id ? "" : <Carousel/>}
             <div className="container">
                 <div className="row my-5">
-                    <ItemList items={items} />
+                    <ItemList items={items} /> 
+
                 </div>
             </div>
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXBMz7T464hExXDE8iSn7z1S1cgQ9QTT0",
+  authDomain: "proyectofinal-react-code.firebaseapp.com",
+  projectId: "proyectofinal-react-code",
+  storageBucket: "proyectofinal-react-code.appspot.com",
+  messagingSenderId: "492846098025",
+  appId: "1:492846098025:web:97c511c24143760d2d64f8"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
